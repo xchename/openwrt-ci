45026e2f912e5cf9c06f2219144548fc912d7c25
xchename <147893443+xchename@users.noreply.github.com>
2026-08-12 00:33:35 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -32,6 +32,7 @@ function git_sparse_clone() {
 # git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
+git clone --depth=1 https://github.com/zxl78585/luci-app-autoreboot package/luci-app-autoreboot
 
 # 科学上网插件
 # git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
