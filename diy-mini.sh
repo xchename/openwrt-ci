c013968d9c3780ae871fb730a714454c9f71c417
xchename <147893443+xchename@users.noreply.github.com>
2026-08-11 15:17:43 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -29,8 +29,8 @@ function git_sparse_clone() {
 }
 
 # 添加额外插件
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
+# git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+# git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
 
 # 科学上网插件
