b465be5889a40939183ff137f1412691c3b593b3
xchename <147893443+xchename@users.noreply.github.com>
2026-08-12 00:16:09 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -45,7 +45,7 @@ git clone --depth=1 https://github.com/vernesong/OpenClash package/luci-app-open
 # git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom-ng
 # git_sparse_clone main https://github.com/haiibo/packages luci-theme-opentomcat
 
 # 更改 Argon 主题背景
